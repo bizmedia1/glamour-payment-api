fe099c4aa2c9f0d649a9bff140404847925e5bae
bizmedia1 <victorisrael539@gmail.com>
2026-06-02 19:37:06 +0100

    Update create-account.js

--- api/create-account.js
+++ api/create-account.js
@@ -1,1 +1,63 @@
+export default async function handler(req, res) {
 
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      message: "Method not allowed"
+    });
+  }
+
+  try {
+
+    const {
+      firstName,
+      lastName,
+      email,
+      phone
+    } = req.body;
+
+    const reference =
+      "GLAM_" +
+      Date.now() +
+      "_" +
+      Math.floor(Math.random() * 100000);
+
+    const response = await fetch(
+      "https://api-v1.aspfiy.com/reserve-paga/",
+      {
+        method: "POST",
+        headers: {
+          "Authorization":
+            `Bearer ${process.env.ASPIFY_SECRET_KEY}`,
+          "Content-Type":
+            "application/json",
+          "accept":
+            "application/json"
+        },
+        body: JSON.stringify({
+          reference,
+          firstName,
+          lastName,
+          email,
+          phone,
+
+          webhookUrl:
+            "https://example.com/webhook"
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
+
+  }
+
+}
